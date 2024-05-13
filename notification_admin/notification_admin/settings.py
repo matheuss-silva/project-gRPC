474dea9c8b484d9326415eeb6a5e49c15c4ad619
Matheus Silva <57456752+matheuss-silva@users.noreply.github.com>
2024-05-13 00:07:54 -0300

    tentantiva de conexão do gRPC-Web

--- notification_admin/notification_admin/settings.py
+++ notification_admin/notification_admin/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -71,9 +70,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Adjust the path to your static directory
-]
 
 WSGI_APPLICATION = 'notification_admin.wsgi.application'
 
@@ -123,9 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
